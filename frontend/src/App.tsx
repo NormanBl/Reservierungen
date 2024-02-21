31f79a3aa116edd4e21170cab9c2d95ea8560d68
NormanBl <norman.blaeker@gmail.com>
2024-02-21 18:50:12 +0100

    backend init / Reservierungsinput gefixt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
-import newReservationForm from './components/newReservationForm';
 import { Form, Button, Card , Container, Navbar, Accordion } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { CalendarComponent , ChangedEventArgs } from '@syncfusion/ej2-react-calendars';
 import NewReservationForm from './components/newReservationForm';
 import ReservationTable from './components/reservationTable';
-//https://ej2.syncfusion.com/react/documentation/api/calendar/overview/
-// das Beispiel zum Kalendar https://ej2.syncfusion.com/react/demos/?utm_source=npm&utm_campaign=calendar#/material/calendar/default
+
 function App(this: any) {
   const [logInStatus , setLogIn] = useState(false);
   const [userName , setUserName] = useState("");
-  const [password , setPassword] = useState("")
-  const [selectedDate , setSelectedDate] = useState(new Date ())
+  const [password , setPassword] = useState("");
+  const [selectedDate , setSelectedDate] = useState(new Date());
 
   function onchange(args: ChangedEventArgs): void {
     setSelectedDate(args.value!!)
@@ -55,7 +53,7 @@ function App(this: any) {
           id='calendar'
           className='middle'
           min={new Date()}
-          value = {new Date()}
+          value = {selectedDate}
           change={onchange}
           />
         <hr/>
